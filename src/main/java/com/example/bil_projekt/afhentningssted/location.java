4058eaa1d4b3f65828d3c19535790f2d6928dae6
mahamed24 <mafa0003@stud.kea.dk>
2025-11-25 14:53:10 +0100

    basel er kurder test

--- src/main/java/com/example/bil_projekt/afhentningssted/location.java
+++ src/main/java/com/example/bil_projekt/afhentningssted/location.java
@@ -1,4 +1,8 @@
 package com.example.bil_projekt.afhentningssted;
 
 public class location {
+    public static void main(String[] args) {
+        String Kurder = "Basel er alem for kodning";
+        System.out.println(Kurder);
+    }
 }
