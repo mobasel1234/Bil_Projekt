6f5953ce68680fcd308191dcf75bafe266be6cae
mobasel1234 <moya0002@stud.kea.dk>
2025-11-25 14:10:10 +0100

    Initial commit

---
+++ src/main/java/com/example/bil_projekt/BilProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.bil_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilProjektApplication.class, args);
+    }
+
+}
