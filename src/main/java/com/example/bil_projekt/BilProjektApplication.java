63ca86df0f93819674797945fac448f863898576
ZoniD <deba0001@stud.kea.dk>
2025-11-26 10:46:22 +0100

    Initial commit

--- src/main/java/com/example/bil_projekt/BilProjektApplication.java
+++ src/main/java/com/example/bil_projekt/BilProjektApplication.java
@@ -3,11 +3,31 @@ package com.example.bil_projekt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 @SpringBootApplication
 public class BilProjektApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BilProjektApplication.class, args);
-    }
 
+        String url = "jdbc:mysql://localhost:3306/DIT_DATABASE?serverTimezone=UTC";
+        String user = "root";
+        String password = "ditPassword";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT 1");
+            if (rs.next()) {
+                System.out.println("Query virker! Database OK.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
